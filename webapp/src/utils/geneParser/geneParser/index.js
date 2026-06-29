46c9efa5031a76ab153d9ded88a245d25fc717ce
Oseremen <o.b.ojiefoh@wustl.edu>
2026-06-29 16:04:31 -0500

    fix: remove accidentally nested geneParser/geneParser directory in webapp

--- webapp/src/utils/geneParser/geneParser/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * geneParser/index.js
- * Barrel export for the gene list parser utilities.
- */
-
-export { detectFormat }      from './detectFormat.js';
-export { suggestScoreColumn } from './scoreColumns.js';
-export { parseGeneList }      from './parseGeneList.js';
-export { resolveIdentifiers } from './resolveIds.js';
